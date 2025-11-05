1d6e2456d99240e2f0d659f22b99d6a154373b9c
Sydney Lunasin <sydney.lunasin@gmail.com>
2025-11-04 20:49:06 -0800

    fix

--- fetal_health.py
+++ fetal_health.py
@@ -4,7 +4,7 @@ import pickle
 import warnings
 import numpy as np
 from pathlib import Path
-import matplotlib as plt
+import matplotlib.pyplot as plt
 warnings.filterwarnings('ignore')
 
 st.title("Fetal Health Classification: A Machine Learning App")
